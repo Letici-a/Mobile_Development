a09fd41e8084b9b245ba74c3f615235f61bf1296
Letici-a <155784235+Letici-a@users.noreply.github.com>
2024-03-27 00:21:16 +0100

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -15,12 +16,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: GetStarted(),
+      home: const Post(),
     );
   }
 }
 
 class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +41,7 @@ class GetStarted extends StatelessWidget {
               child: Align(
                 alignment: Alignment.topRight,
                 child: Transform.translate(
-                  offset: Offset(58, -20),
+                  offset: const Offset(58, -20),
                   child: Image.asset('assets/images/Group.png'),
                 ),
               ),
@@ -47,7 +50,7 @@ class GetStarted extends StatelessWidget {
               child: Align(
                 alignment: Alignment.bottomLeft,
                 child: Transform.translate(
-                  offset: Offset(-105, 70),
+                  offset: const Offset(-105, 70),
                   child: Image.asset('assets/images/Frame.png'),
                 ),
               ),
@@ -56,7 +59,7 @@ class GetStarted extends StatelessWidget {
               child: Align(
                 alignment: Alignment.center,
                 child: Transform.translate(
-                  offset: Offset(0, -160),
+                  offset: const Offset(0, -160),
                   child: Transform.scale(
                     scale: 0.75,
                     child: Image.asset('assets/images/first.png'),
@@ -68,8 +71,8 @@ class GetStarted extends StatelessWidget {
               child: Align(
                 alignment: Alignment.center,
                 child: Transform.translate(
-                  offset: Offset(5, 10),
-                  child: Column(
+                  offset: const Offset(5, 10),
+                  child: const Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       Text(
@@ -99,12 +102,12 @@ class GetStarted extends StatelessWidget {
               child: Align(
                 alignment: Alignment.center,
                 child: Transform.translate(
-                  offset: Offset(0, 245),
+                  offset: const Offset(0, 245),
                   child: ElevatedButton(
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => Home()),
+                        MaterialPageRoute(builder: (context) => const Home()),
                       );
                     },
                     style: ElevatedButton.styleFrom(
@@ -113,7 +116,7 @@ class GetStarted extends StatelessWidget {
                         borderRadius: BorderRadius.circular(15),
                       ),
                     ),
-                    child: Padding(
+                    child: const Padding(
                       padding: EdgeInsets.only(
                           left: 35.0, top: 15.0, right: 35.0, bottom: 15.0),
                       child: Text(
@@ -138,10 +141,147 @@ class GetStarted extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child: const Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Text(
+                    'The Latest',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 10,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF3F6FE3),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Post(),
+            Post(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Post extends StatelessWidget {
+  const Post({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: const Color(0xFF396EE2),
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(7.0),
+          ),
+          child: Wrap(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(18),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Transform.scale(
+                            scale: 0.1,
+                            child: Image.asset('assets/images/first.png'),
+                          ),
+                        ),
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Dash',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 10,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              '@dash.dash',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 6,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0xFF9E9E9E),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: Text(
+                      '🎓 Exciting news! I\'m now a student at Flutter Training, learning more about mobile development with Flutter. Can\'t wait to gain new skills and become a skilled Flutter developer!',
+                      style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 10,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Image.asset('assets/images/image 1.png'),
+                  Padding(
+                    padding: const EdgeInsets.all(18),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Image.asset('assets/images/Frame 4.png'),
+                              const SizedBox(width: 25),
+                              Image.asset('assets/images/Frame 5.png'),
+                              const SizedBox(width: 25),
+                              Image.asset('assets/images/Frame 6.png'),
+                            ],
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child:
+                              Image.asset('assets/images/BookmarkSimple.png'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
